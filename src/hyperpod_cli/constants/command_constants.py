18bb566acaba128684ddd4d728261b114ea25a58
Zhaoqi <zhaoqiwang.baruch@gmail.com>
2025-03-26 09:53:08 -0700

    Add a new custom label to store user name (#71)
    **Description**
    Introduced new label "sagemaker.user/created-by" with value user/role name from sts.get_caller_identity() response
    
    **Testing Done**
    Tested in beta account and can see the label added

--- src/hyperpod_cli/constants/command_constants.py
+++ src/hyperpod_cli/constants/command_constants.py
@@ -46,6 +46,7 @@ SAGEMAKER_TRAINING_LAUNCHER_DIR = str(Path(__file__).parent.parent / "sagemaker_
 NVIDIA_GPU_RESOURCE_LIMIT_KEY = "nvidia.com/gpu"
 AVAILABLE_ACCELERATOR_DEVICES_KEY = "AvailableAcceleratorDevices"
 TOTAL_ACCELERATOR_DEVICES_KEY = "TotalAcceleratorDevices"
+USER_NAME_LABEL_KEY = "sagemaker.user/created-by"
 
 class PullPolicy(Enum):
     ALWAYS = "Always"
