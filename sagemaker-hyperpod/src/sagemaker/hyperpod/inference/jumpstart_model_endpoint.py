984c8208451d056257735f554a66a1405d52b586
jzhaoqwa <jzhaoqwa@amazon.com>
2025-06-12 10:30:06 -0700

    Refactor create inference function
    **Description**
    
    Refactor ModelEndpoint classes to let create happen in separate method instead of constructor
    
    **Testing Done**
    
    Manually tested in demo notebook

--- sagemaker-hyperpod/src/sagemaker/hyperpod/inference/jumpstart_model_endpoint.py
+++ sagemaker-hyperpod/src/sagemaker/hyperpod/inference/jumpstart_model_endpoint.py
@@ -5,31 +5,66 @@ from datetime import datetime
 
 
 class JumpStartModelEndpoint(ModelEndpointBase):
-    def __init__(
+    def _validate_inputs(
+            self,
+            model_id: str,
+            instance_type: str,
+            spec: JumpStartModelSpec,
+        ):
+        # Check if spec is provided, other parameters should be None
+        if spec is not None:
+            if model_id or instance_type:
+                raise ValueError("Either spec or model_id and instance_type must be provided, but not both.")
+            if not isinstance(spec, JumpStartModelSpec):
+                raise TypeError(f"spec must be of type JumpStartModelSpec, got {type(spec)}")
+            return
+
+        # Validate required parameters when spec is None
+        if model_id is None or instance_type is None:
+            raise ValueError("Must provide both model_id and instance_type.")
+        
+        # Type validation
+        if not isinstance(model_id, str):
+            raise TypeError(f"model_id must be of type str, got {type(model_id)}")
+        if not isinstance(instance_type, str):
+            raise TypeError(f"instance_type must be of type str, got {type(instance_type)}")
+
+    def _validate_instance_type(self):
+        '''
+        Get supported instance types from node and model
+        '''
+        pass
+
+    def _get_default_endpoint_name(self, model_id):
+        '''
+        Generate default endpoint name if not specified by user
+        '''
+        time_str = datetime.now().strftime("%y%m%d-%H%M%S-%f")
+        
+        return model_id + '-' + time_str
+    
+    def create(
         self,
-        namespace: str = 'default',
+        namespace: str,
         model_id: str = None,
         instance_type: str = None,
-        sagemaker_endpoint: str = None,
         spec: JumpStartModelSpec = None,
     ):
+        self._validate_inputs(model_id, instance_type, spec)
+
         if spec is None:
+            sagemaker_endpoint = self._get_default_endpoint_name(model_id)
+            
+            # create spec config
             spec = JumpStartModelSpec(
                 model=Model(model_id=model_id),
                 server=Server(instance_type=instance_type),
                 sage_maker_endpoint=SageMakerEndpoint(name=sagemaker_endpoint),
             )
 
-        super().__init__(
+        self.invoke_api_call(
             name=spec.model.modelId,    # use model id as metadata name
             kind=JUMPSTART_MODEL_KIND,
             namespace=namespace,
-            spec=spec
+            spec=spec,
         )
-        
-        '''
-        if not self.spec.sageMakerEndpoint.name :
-            time_str = datetime.now().strftime("%y%m%d-%H%M%S-%f")
-            endpoint_name = self.spec.model.modelId + '-' + time_str
-            self.spec.sage_maker_endpoint.name = endpoint_name
-        '''
--- sagemaker-hyperpod/src/sagemaker/hyperpod/inference/model_endpoint_base.py
+++ sagemaker-hyperpod/src/sagemaker/hyperpod/inference/model_endpoint_base.py
@@ -8,22 +8,6 @@ from types import SimpleNamespace
 
 
 class ModelEndpointBase:
-    def __init__(self,
-            name: str,
-            kind: str,
-            namespace: str,
-            spec: JumpStartModelSpec | InferenceEndpointConfigSpec,
-        ):
-        self.apiVersion = INFERENCE_API_VERSION
-        self.kind = kind
-
-        self.metadata = SimpleNamespace(
-            name=name,
-            namespace=namespace
-        )
-
-        self.spec = spec
-
     def _validate_cluster_connection(self):
         try:
             k8s_config.load_kube_config()
@@ -31,31 +15,36 @@ class ModelEndpointBase:
             return True
         except Exception as e:
             return False
-
-    def config(self):
-        return {
-            "apiVersion": self.apiVersion,
-            "kind": self.kind,
-            "metadata": self.metadata.__dict__,
-            "spec": self.spec.model_dump(exclude_none=True)
-        }
     
-    def deploy(self):
+    def invoke_api_call(
+            self,
+            name: str,
+            kind: str,
+            namespace: str,
+            spec: JumpStartModelSpec | InferenceEndpointConfigSpec,
+        ):
         if not self._validate_cluster_connection():
             raise Exception("Failed to connect to the Kubernetes cluster. Please check your kubeconfig.")
         
         custom_api = client.CustomObjectsApi()
-        print('\n Deploying with dict:', self.config())
+
+        body = {
+            "apiVersion": INFERENCE_API_VERSION,
+            "kind": kind,
+            "metadata": SimpleNamespace(name=name, namespace=namespace).__dict__,
+            "spec": spec.model_dump(exclude_none=True)
+        }
+        print('\n Deploying with dict:', body)
 
         try:
             custom_api.create_namespaced_custom_object(
                 group="inference.sagemaker.aws.amazon.com",
                 version="v1alpha1",
-                namespace=self.metadata.namespace,
-                plural=KIND_PLURAL_MAP[self.kind],
-                body=self.config(),
+                namespace=namespace,
+                plural=KIND_PLURAL_MAP[kind],
+                body=body,
             )
-            print('Successful deployed model and its endpoint!')
+            print('\nSuccessful deployed model and its endpoint!')
         except Exception as e:
-            print(f"Failed to deploy model and its endpoint: {e}")
+            print(f"\nFailed to deploy model and its endpoint: {e}")
 
