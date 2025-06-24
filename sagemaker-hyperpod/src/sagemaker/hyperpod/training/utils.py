d3db0af91d6bff242c41dbaabbc3a9f0e985f2dc
pintaoz-aws <167920275+pintaoz-aws@users.noreply.github.com>
2025-06-24 13:59:25 -0700

    Add HyperpodPytorchJob class (#39)
    * Add HyperpodPytorchJob
    
    * update to class methods
    
    * update to class methods
    
    * Address feedback
    
    * Fix bug
    
    ---------
    
    Co-authored-by: pintaoz <pintaoz@amazon.com>

---
+++ sagemaker-hyperpod/src/sagemaker/hyperpod/training/utils.py
@@ -0,0 +1,21 @@
+from kubernetes import client
+from kubernetes import config as k8s_config
+
+
+def validate_cluster_connection():
+    try:
+        k8s_config.load_kube_config()
+        v1 = client.CoreV1Api()
+        return True
+    except Exception as e:
+        return False
+
+def remove_metadata(hp_job: dict):
+    hp_job.pop('metadata')
+    return hp_job
+
+def remove_metadata_from_list(hp_job_list):
+    hp_job_list.pop('metadata')
+    for hp_job in hp_job_list['items']:
+        hp_job = remove_metadata(hp_job)
+    return hp_job_list
