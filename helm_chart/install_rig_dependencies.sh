31abeed1a5eb651b02d4629ff0eb3ecdc5ae1b3d
Chris Chan <chnnmz@amazon.com>
2025-06-25 20:11:27 +0000

    Add EFA patch for tolerations (daemonset)

--- helm_chart/install_rig_dependencies.sh
+++ helm_chart/install_rig_dependencies.sh
@@ -2,7 +2,16 @@
 
 SRC_DIR="HyperPodHelmChart"
 OUTPUT_DIR="HyperPodHelmChartForRIG"
-TRAINING_OPERATORS=dependencies-training-operators # dependencies- prefix from standard Helm installation
+STANDARD_HELM_RELEASE_NAME=dependencies
+TRAINING_OPERATORS=$STANDARD_HELM_RELEASE_NAME-training-operators # dependencies- prefix from standard Helm installation
+EFA=$STANDARD_HELM_RELEASE_NAME-aws-efa-k8s-device-plugin         # dependencies- prefix from standard Helm installation
+PATCH_ONLY=(
+    #
+    # These objects do not need entirely separate YAML; we just need to patch them to make them work with RIG
+    #
+    "$TRAINING_OPERATORS"
+    "$EFA"
+)
 
 # Format: "<eks|hyperpod>,namespace,<k8s_name|chart_dir>"
 add_ons=(
@@ -11,6 +20,7 @@ add_ons=(
     #"hp,kube-system,mpi-operator,deployment"
     #"hp,kube-system,neuron-device-plugin,daemonset"
     "hp,kubeflow,$TRAINING_OPERATORS,deployment"
+    "hp,kube-system,$EFA,daemonset"
 )
 
 generate_helm_chart_root() {
@@ -47,7 +57,7 @@ get_helm_chart_from_local() {
         k="DaemonSet"
     fi
 
-    helm template dependencies $srcdir/charts/$chart -f $srcdir/values.yaml -f $srcdir/charts/$chart/values.yaml --debug | \
+    helm template $STANDARD_HELM_RELEASE_NAME $srcdir/charts/$chart -f $srcdir/values.yaml -f $srcdir/charts/$chart/values.yaml --debug | \
         yq "select(.kind == \"$k\")" -
 }
 
@@ -236,6 +246,52 @@ override_training_operators() {
     ]'
 }
 
+override_efa() {
+    #####################################################
+    # aws-efa-k8s-device-plugin dependency needs to be present
+    # for multi-node training but by by default, only
+    # tolerates non-RIG nodes
+    #
+    # Therefore, needs to tolerate RIG node taint.
+    #
+    # NOTE: this based on the original Helm installation
+    #       of aws-efa-k8s-device-plugin Deployment.
+    #       There are tolerations
+    #       as of commit 
+    #       https://github.com/aws/sagemaker-hyperpod-cli/blob/9ff002f949bc408849f7673678f46a3326983ed2/helm_chart/HyperPodHelmChart/values.yaml#L244
+    #
+    #       Therefore, we need to append NOT replace, but append.
+    #       This is done using the `-` at the end of the `path`
+    #####################################################
+   
+    # Using kubectl directly since relatively simple patch 
+    # that does not require new separeate file/deployments specific for RIG
+    kubectl patch daemonset $EFA -n kube-system --type=json -p='[
+      {
+        "op": "add",
+        "path": "/spec/template/spec/tolerations/-",
+        "value": {
+          "key": "sagemaker.amazonaws.com/RestrictedNode",
+          "value": "Worker",
+          "effect": "NoSchedule"
+        }
+      }
+    ]'
+}
+
+# Function to check if an item is in the skiplist
+in_skiplist() {
+    local item="$1"
+    shift
+    local -a skiplist=("$@")
+    for skip in "${skiplist[@]}"; do
+        if [[ "$item" == "$skip" ]]; then
+            return 0  # True, item is in skiplist
+        fi
+    done
+    return 1  # False, item is not in skiplist
+}
+
 fetch_yaml_and_enable_overrides() {
     local resources=("${!1}")
     
@@ -250,7 +306,7 @@ fetch_yaml_and_enable_overrides() {
         #####################################################
         # Clean and (Re)Create Helm chart directories
         #####################################################
-	if [ "$name" != "$TRAINING_OPERATORS" ]; then
+	if ! in_skiplist "$name" "${PATCH_ONLY[@]}" ; then
             rm -rf $OUTPUT_DIR/charts/$name/templates
             rm -f $OUTPUT_DIR/charts/$name/*.tgz
             mkdir -p $OUTPUT_DIR/charts/$name/templates
@@ -276,8 +332,7 @@ fetch_yaml_and_enable_overrides() {
 		    enable_nodeselectors_and_tolerations_overrides $outpath
             fi
 	else
-	    if [ "$name" = "$TRAINING_OPERATORS" ]; then
-		# training-operators is a special case with different requirements
+	    if in_skiplist "$name" "${PATCH_ONLY[@]}"; then
 		continue
             else
                 cp -r $SRC_DIR/charts/$name/. $OUTPUT_DIR/charts/$name
@@ -298,6 +353,7 @@ assert_addons_enabled() {
         response=$(kubectl get $kind $name -n $namespace --no-headers 2>&1)
         if [[ "$response" == *"Error from server (NotFound)"* ]] || [ -z "$response" ]; then
             echo "Namespace $namespace does not exist or No $kind $name found in namespace $namespace. Please ensure CNI, CoreDNS add-ons enabled, and that standard HyperPod Helm chart is installed for this cluster before installing RIG dependencies."
+	    echo "⚠️ Note: RIG installation depends on the Helm release name. If the standard Helm installation command/release name changed (i.e. no longer `helm install dependencies...` where `dependencies` is the release name), then please update the RELEASE_NAME in install_rig_dependencies.sh before running"
             exit 1
         fi
     done
@@ -326,26 +382,33 @@ confirm_installation_with_user() {
       echo "🔧 Installing Helm chart..."
       helm upgrade --install rig-dependencies ./HyperPodHelmChartForRIG --namespace kube-system -f ./HyperPodHelmChartForRIG/values.yaml
       if [ $? -ne 0 ]; then
-        echo "RIG Helm Installation Failed. Exiting (0/3 steps completed)..."
+        echo "RIG Helm Installation Failed. Exiting (0/4 steps completed)..."
         return 1
       fi
   
       # aws-node needs specific instllation for *.nonrig.yaml
       kubectl apply -f HyperPodHelmChartForRIG/charts/aws-node/templates/daemonset.nonrig.yaml -n kube-system
       if [ $? -ne 0 ]; then
-        echo "RIG Helm Installation Failed (aws-node). Exiting (only 1/3 steps completed)..."
+        echo "RIG Helm Installation Failed (aws-node). Exiting (only 1/4 steps completed)..."
         return 1
       fi
 
       # training-operator needs specific patch
       override_training_operators
       if [ $? -ne 0 ]; then
-        echo "RIG Helm Installation Failed (training-operator). Exiting (only 2/3 steps completed)..."
+        echo "RIG Helm Installation Failed (training-operator). Exiting (only 2/4 steps completed)..."
+        return 1
+      fi
+
+      # efa needs specific patch
+      override_efa
+      if [ $? -ne 0 ]; then
+        echo "RIG Helm Installation Failed (aws-efa-k8s-device-plugin). Exiting (only 3/4 steps completed)..."
         return 1
       fi
 
       echo ""
-      echo "RIG Helm Installation Succeeded (3/3 steps completed)."
+      echo "RIG Helm Installation Succeeded (4/4 steps completed)."
       echo ""
 
       # Warn user about CNI start up
