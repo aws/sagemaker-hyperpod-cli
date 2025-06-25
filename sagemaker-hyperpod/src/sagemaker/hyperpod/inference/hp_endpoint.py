574351e13d9f8b28e72b12d2379656eee03c8c4d
jzhaoqwa <jzhaoqwa@amazon.com>
2025-06-24 17:02:40 -0700

    Add tlsConfig to quick create

--- sagemaker-hyperpod/src/sagemaker/hyperpod/inference/hp_endpoint.py
+++ sagemaker-hyperpod/src/sagemaker/hyperpod/inference/hp_endpoint.py
@@ -8,6 +8,7 @@ from sagemaker.hyperpod.inference.config.hp_endpoint_config import (
     ModelInvocationPort,
     ModelVolumeMount,
     Resources,
+    TlsConfig,
 )
 from sagemaker.hyperpod.inference.hp_endpoint_base import HPEndpointBase
 from datetime import datetime
@@ -137,6 +138,7 @@ class HPEndpoint(HPEndpointBase):
         endpoint_name: str = None,
         model_volume_mount_name: str = None,
         model_volume_mount_path: str = None,
+        tls_s3_uri: str = None,
     ):
         instance = cls()
 
@@ -159,6 +161,9 @@ class HPEndpoint(HPEndpointBase):
 
         if not model_volume_mount_path:
             model_volume_mount_path = DEFAULT_MOUNT_PATH
+
+        if not tls_s3_uri:
+            tls_s3_uri = ''
 
         if model_source_type == "s3":
             model_source_config = ModelSourceConfig(
@@ -188,6 +193,10 @@ class HPEndpoint(HPEndpointBase):
             resources=Resources(),
         )
 
+        tls_config = TlsConfig(
+            tls_certificate_output_s3_uri=tls_s3_uri,
+        )
+
         # create spec config
         spec = InferenceEndpointConfigSpec(
             instance_type=instance_type,
@@ -196,6 +205,7 @@ class HPEndpoint(HPEndpointBase):
             model_source_config=model_source_config,
             worker=worker,
             endpoint_name=endpoint_name,
+            tls_config=tls_config,
         )
 
         instance.call_create_api(
@@ -248,9 +258,10 @@ class HPEndpoint(HPEndpointBase):
         )
 
         output_data = []
-        for item in response["items"]:
-            metadata = item["metadata"]
-            output_data.append((metadata["name"], metadata["creationTimestamp"]))
+        if response and response["items"]:
+            for item in response["items"]:
+                metadata = item["metadata"]
+                output_data.append((metadata["name"], metadata["creationTimestamp"]))
         headers = ["METADATA NAME", "CREATE TIME"]
 
         print(tabulate(output_data, headers=headers))
