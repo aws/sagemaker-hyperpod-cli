bde684b6912a6eefba35067a969ffebf7b0dc68f
Roja Reddy Sareddy <rsareddy@amazon.com>
2025-06-29 08:23:21 -0700

    Training CLI for Launch

--- sagemaker-hyperpod/src/sagemaker/hyperpod/cli/constants/hp_pytorch_command_constants.py
+++ sagemaker-hyperpod/src/sagemaker/hyperpod/cli/constants/hp_pytorch_command_constants.py
@@ -3,14 +3,26 @@ HyperPod PyTorch Job CLI
 Find more information at: https://docs.aws.amazon.com/sagemaker/latest/dg/sagemaker-hyperpod.html
 Commands:
   * create         Create and submit a PyTorch training job
-                   Using either config file or command line parameters
+
+  * list           List PyTorch training jobs
+
+  * describe       Describe a PyTorch training job
+
 
 Examples:
-  
+
   Create and submit job:
-    hp hp-pytorch-job create --job-name "my_pytorch_job"  --image "my_image"
+    hyp create hp-pytorch-job  --job-name "my_pytorch_job"  --image "my_image"
+  List Jobs:
+    hyp list hp-pytorch-job
+  Describe Job
+    hyp describe hp-pytorch-job --job-name "my_pytorch_job"
+
 
 Usage:
   hp create hp-pytorch-job [options]
 Use "hp create hp-pytorch-job --help" for more information about the command.
+Use "hp list hp-pytorch-job --help" for more information about the command.
+Use "hyp describe hp-pytorch-job --help" for more information about the command.
+
 """
