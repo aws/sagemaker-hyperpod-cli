906ac349199d9936d4ed4bf6ea5c243a72e7c746
jiapinw <95885824+jiapinw@users.noreply.github.com>
2025-10-13 10:20:41 -0700

    Initial commit for kv-cache and intelligent support in inference SDK/CLI (#252)
    * Initial commit for kv-cache and intelligent support in inference SDK
    
    * Initial commit for kv-cache & intelligent routing CLI changes
    
    * [Fix] clean up test code
    
    * Separate out kv-cache and intelligent routing spec update into v1_1 inference cli
    
    * Fix ut and black format
    
    * Fix template.py extra white lines

--- hyperpod-custom-inference-template/hyperpod_custom_inference_template/registry.py
+++ hyperpod-custom-inference-template/hyperpod_custom_inference_template/registry.py
@@ -10,13 +10,18 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-from hyperpod_custom_inference_template.v1_0 import model as v1
-from hyperpod_custom_inference_template.v1_0.template import TEMPLATE_CONTENT as v1_template
+from hyperpod_custom_inference_template.v1_0 import model as v1_0
+from hyperpod_custom_inference_template.v1_1 import model as v1_1
+from hyperpod_custom_inference_template.v1_0.template import (
+    TEMPLATE_CONTENT as v1_0_template,
+)
+from hyperpod_custom_inference_template.v1_1.template import (
+    TEMPLATE_CONTENT as v1_1_template,
+)
 
 SCHEMA_REGISTRY = {
-    "1.0": v1.FlatHPEndpoint,
+    "1.0": v1_0.FlatHPEndpoint,
+    "1.1": v1_1.FlatHPEndpoint,
 }
 
-TEMPLATE_REGISTRY = {
-    "1.0": v1_template
-}
+TEMPLATE_REGISTRY = {"1.0": v1_0_template, "1.1": v1_1_template}
