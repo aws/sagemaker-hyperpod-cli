99c4705d8d6ced150a30e85d48128c613f2f5cc0
Brian Xia <brianfruitose@gmail.com>
2025-10-21 11:35:17 -0700

    Add dev_space_constants.py (#255)
    Co-authored-by: Brian Xia <brianxia@amazon.com>

---
+++ src/sagemaker/hyperpod/cli/constants/dev_space_constants.py
@@ -0,0 +1,20 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+DEV_SPACE_GROUP = "sagemaker.aws.com"
+DEV_SPACE_VERSION = "v1alpha1"
+DEV_SPACE_PLURAL = "spaces"
+DEFAULT_DEV_SPACE_PORT = "8888"
+# Immutable fields that cannot be updated after dev space creation
+IMMUTABLE_FIELDS = {
+    "storage_class_name",
+}
