c98fd6e41b2d14e2ccc723f44d47846f25a177b8
Keita Watanabe <keitaw09@gmail.com>
2026-03-27 12:20:53 +0000

    Add MIG profile support for ml.p6-b300.48xlarge (Blackwell Ultra)
    Add ml.p6-b300.48xlarge to INSTANCE_TYPE_MIG_PROFILES in constants.py
    with the correct B300 MIG profiles derived from the NVIDIA GPU Operator
    v25.3.0 upstream ConfigMap (device-filter 0x318210DE):
    
    - mig-1g.34gb, mig-1g.67gb, mig-2g.67gb
    - mig-3g.135gb, mig-4g.135gb, mig-7g.269gb
    
    Also add the corresponding uniform and mixed MIG partition profiles
    to the Helm chart default-mig-config.yaml ConfigMap, following the
    same pattern used for existing GPU types (H100, H200, B200).
    
    The B300 GPU (288GB HBM3e, ~269GB usable) was already registered in
    INSTANCE_RESOURCES but had no MIG profile mapping, causing HyperPod
    MIG validation to reject accelerator partition requests on this
    instance type.

--- src/sagemaker/hyperpod/training/constants.py
+++ src/sagemaker/hyperpod/training/constants.py
@@ -132,6 +132,7 @@ INSTANCE_TYPE_MIG_PROFILES = {
     'ml.p5e.48xlarge': ['mig-1g.18gb', 'mig-1g.35gb', 'mig-2g.35gb', 'mig-3g.71gb', 'mig-4g.71gb', 'mig-7g.141gb'],
     'ml.p5en.48xlarge': ['mig-1g.18gb', 'mig-1g.35gb', 'mig-2g.35gb', 'mig-3g.71gb', 'mig-4g.71gb', 'mig-7g.141gb'],
     'p6-b200.48xlarge': ['mig-1g.23gb', 'mig-1g.45gb', 'mig-2g.45gb', 'mig-3g.90gb', 'mig-4g.90gb', 'mig-7g.180gb'],
+    'ml.p6-b300.48xlarge': ['mig-1g.34gb', 'mig-1g.67gb', 'mig-2g.67gb', 'mig-3g.135gb', 'mig-4g.135gb', 'mig-7g.269gb'],
     'ml.p6e-gb200.36xlarge': ['mig-1g.23gb', 'mig-1g.47gb', 'mig-2g.47gb', 'mig-3g.93gb', 'mig-4g.93gb', 'mig-7g.186gb'],
     'ml.g7e.2xlarge': ['mig-1g.24gb', 'mig-2g.48gb', 'mig-4g.96gb'],
     'ml.g7e.4xlarge': ['mig-1g.24gb', 'mig-2g.48gb', 'mig-4g.96gb'],
